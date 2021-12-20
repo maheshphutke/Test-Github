8e9397f0ce2d9c2610617a022ab7a88c2ce1c944
Dell <Dell@DESKTOP-9UQDRB3.mshome.net>
2021-12-20 14:16:43 +0530

    push to github

---
+++ src/aug_21_git_test1/test_git.java
@@ -0,0 +1,23 @@
+package aug_21_git_test1;
+
+public class test_git {
+
+	public static void main(String[] args) {
+
+		int i, j;
+
+		for (i = 1; i <= 6; i++)
+
+		{
+			for (j = 1; j <= 6; j++)
+
+			{
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
