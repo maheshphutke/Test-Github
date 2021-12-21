581498ee0e57e31a8524cc9e88088cb45073085f
Dell <Dell@DESKTOP-9UQDRB3.mshome.net>
2021-12-21 12:34:58 +0530

    newly added

---
+++ src/aug_21_git_test1/multilpevariable.java
@@ -0,0 +1,22 @@
+package aug_21_git_test1;
+
+public class multilpevariable {
+	public static void main(String[] args) {
+        String name;          //  variable Declaration
+        int rollno;           // variable Declaration
+        float marks;          // variable Declaration 
+        char grade;          // variable Declaration
+        
+        name="mahesh";      // Variable initialization
+        rollno=109;
+        marks=67.33f;
+        grade='A';
+        
+        System.out.println ("my name is"+name);      // variable usage
+        System.out.println ("my roll number is"+rollno);
+        System.out.println ("mymarks are"+marks);
+        System.out.println ("my grade is"+grade);
+                            
+	}
+
+}
---
+++ src/aug_21_git_test1/single_variable.java
@@ -0,0 +1,16 @@
+package aug_21_git_test1;
+
+public class single_variable {
+
+	public static void main(String[] args) {
+         int birthdate;
+         birthdate=10;
+         System.out.println("my birthdate is december "+birthdate);
+        
+int a=10; 
+int b=20;
+int sum=a+b;
+System.out.println("my manager "+sum);
+System.out.println("my boss "+sum);
+}
+}
